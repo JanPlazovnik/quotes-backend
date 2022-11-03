fc6db70c8ef1e8c8301a06c7c07b30c9e6e2733c
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-11-03 22:09:38 +0100

    Removed claims from jwt token

--- config/jwt.php
+++ config/jwt.php
@@ -136,12 +136,12 @@ return [
     */
 
     'required_claims' => [
-        'iss',
+        // 'iss',
         'iat',
         'exp',
-        'nbf',
-        'sub',
-        'jti',
+        // 'nbf',
+        // 'sub',
+        // 'jti',
     ],
 
     /*
