7dbb20cb9b2e5eb4c15a2bb83d7f5fca39863136
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-11-04 00:47:57 +0100

    Added a password_confirmation field

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,7 +58,7 @@ class AuthController extends Controller
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:6|confirmed',
         ]);
 
         if ($validator->fails()) {
