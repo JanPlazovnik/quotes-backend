3d5dc193d16064db6297b1ef4944f04624d75333
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-11-04 13:54:15 +0100

    Moved quote logic to QuoteController and added delete

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quote;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class QuoteController extends Controller
 {
@@ -52,6 +54,103 @@ class QuoteController extends Controller
         return response()->json([
             'status' => 'success',
             'data' => $quote,
+        ]);
+    }
+
+    // Post a quote
+    public function postQuote(Request $request)
+    {
+        $validator = $this->getQuoteValidator($request);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'data' => $validator->errors(),
+            ], 422);
+        }
+
+        $user = User::find(auth()->user()->id);
+        $quote = $user->quotes()->create($request->only('content'));
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $quote,
+        ]);
+    }
+
+    // Edit quote
+    public function editQuote(Request $request, $id)
+    {
+        $validator = $this->getQuoteValidator($request);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'data' => $validator->errors(),
+            ], 422);
+        }
+
+        // Fetch the quote
+        $quote = Quote::find($id);
+
+        // Check if quote exists
+        if (!$quote) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Quote not found',
+            ], 404);
+        }
+
+        // Check if user is the author of the quote
+        if ($quote->user_id !== auth()->user()->id) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        // Update the quote text
+        $quote->content = $request->input('content');
+        $quote->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $quote,
+        ]);
+    }
+
+    // Delete quote
+    public function deleteQuote(Request $request, $id)
+    {
+        // Fetch the quote
+        $quote = Quote::find($id);
+
+        // Check if quote exists
+        if (!$quote) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Quote not found',
+            ], 404);
+        }
+
+        // Check if user is the author of the quote
+        if ($quote->user_id !== auth()->user()->id) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $quote->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Quote deleted',
+        ]);
+    }
+
+    private function getQuoteValidator(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'content' => 'required|string|max:255',
         ]);
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Quote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
@@ -21,72 +20,6 @@ class UserController extends Controller
         return response()->json([
             'status' => 'success',
             'data' => auth()->user()
-        ]);
-    }
-
-    // Post a quote
-    public function postQuote(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'content' => 'required|string|max:255',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 'error',
-                'data' => $validator->errors(),
-            ], 422);
-        }
-
-        $user = User::find(auth()->user()->id);
-        $quote = $user->quotes()->create($request->only('content'));
-
-        return response()->json([
-            'status' => 'success',
-            'data' => $quote,
-        ]);
-    }
-
-    // Edit quote
-    public function editQuote(Request $request, $id)
-    {
-        $validator = Validator::make($request->all(), [
-            'content' => 'required|string|max:255',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 'error',
-                'data' => $validator->errors(),
-            ], 422);
-        }
-
-        // Fetch the quote
-        $quote = Quote::find($id);
-
-        // Check if quote exists
-        if (!$quote) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Quote not found',
-            ], 404);
-        }
-
-        // Check if user is the author of the quote
-        if ($quote->user_id !== auth()->user()->id) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Unauthorized',
-            ], 401);
-        }
-
-        // Update the quote text
-        $quote->content = $request->input('content');
-        $quote->save();
-
-        return response()->json([
-            'status' => 'success',
-            'data' => $quote,
         ]);
     }
 
