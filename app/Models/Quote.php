938f395f7e3e77919315b78d0636ab2324a72ab6
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-11-04 17:40:07 +0100

    Added associations for votes

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -22,4 +22,9 @@ class Quote extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasMany(Quote::class);
     }
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class);
+    }
 }
